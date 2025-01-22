aaf85baa1124c46607fc30a3691a0bd70579fd36
Jeevan Gopinath <167789752+Shadowgrafity@users.noreply.github.com>
2025-01-21 20:20:02 -0700

    Add files via upload

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+local M = {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },  
+    })
+    end,
+}
+
+return { M }
